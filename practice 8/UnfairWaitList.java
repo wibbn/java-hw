e0592adb0668040370c9f9765a9bbc530260e1eb
Ivan Lazunin <code.wibbn@gmail.com>
2020-10-14 22:30:41 +0300

    add practice 8

---
+++ practice 8/UnfairWaitList.java
@@ -0,0 +1,13 @@
+public class UnfairWaitList<E> extends WaitList<E>{
+    public UnfairWaitList() {
+        super();
+    }
+
+    public void remove(E element) {
+        super.content.remove(element);
+    }
+
+    public void moveToBack() {
+        super.content.add(super.content.poll());
+    }
+}

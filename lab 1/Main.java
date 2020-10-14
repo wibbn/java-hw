68f62f34234548779452f41382b1510d4f95b343
Ivan Lazunin <code.wibbn@gmail.com>
2020-10-14 23:10:41 +0300

    add lab 1

---
+++ lab 1/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public void factorial(int n) {
+        int res = 1;
+        for (int i=2; i<=n; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
+    public void main() {
+        System.out.println(factorial(10));
+    }
+}

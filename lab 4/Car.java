b8e84ac274bc3326a0349cc4d5af5f7e4245cf19
Ivan Lazunin <code.wibbn@gmail.com>
2020-10-21 21:27:27 +0300

    add lab 4

---
+++ lab 4/Car.java
@@ -0,0 +1,11 @@
+public class Car implements Pricable {
+    private int price;
+    public Car(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ lab 4/Pricable.java
@@ -0,0 +1,3 @@
+public interface Pricable {
+    public int getPrice();
+}
